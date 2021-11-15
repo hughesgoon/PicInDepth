2b56ed65eb3499803c5c8fb08243b508494db50c
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2021-11-15 15:03:20 +0000

    chore: Set vue.config for build
    Set vue.config for build with gh-pages

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+    ? '/picindepth'
+    : '/',
+    outputDir: 'dist',
+	devServer: {
+		disableHostCheck: true,
+        host: '0.0.0.0',
+        port: 8080
+    }
+}
