1a8d30b58a5f3d462ee12fdc567a65cb01a34a0e
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2021-11-16 05:09:15 +0000

    chore: Change tab to space in vue.config
    Change tab to space in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
+    publicPath: process.env.NODE_ENV === 'production'
     ? '/picindepth'
     : '/',
     outputDir: 'dist',
-	devServer: {
-		disableHostCheck: true,
+    devServer: {
+        disableHostCheck: true,
         host: '0.0.0.0',
         port: 8080
     }
