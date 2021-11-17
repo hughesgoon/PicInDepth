fe912b5f9632c1d07f2116d5cb8584cc5eb31d0a
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2021-11-17 14:14:43 +0000

    feat: Add vue-fullpage
    Add vue-fullpage and make guide template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <full-page>
+      <div class="section">
+        Introduce
+      </div>
+      <div class="section">
+        Image Upload
+      </div>
+      <div class="section">
+        waiting
+      </div>
+      <div class="section">
+        making + share
+      </div>
+      <div class="section">
+        TMI
+      </div>
+	</full-page>
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
